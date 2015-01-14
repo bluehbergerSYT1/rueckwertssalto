4e28200cd748f403bad3ce30d96ac95c56099ac3
Lukas Zainzinger <lzainzinger@student.tgm.ac.at>
2015-01-14 10:37:12 +0100

    Verbindung und Tabel auslesen

---
+++ Rueckwaertssalto/src/bluehberger_zainziger/JDBCController.java
@@ -0,0 +1,97 @@
+package bluehberger_zainziger;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.mysql.jdbc.DatabaseMetaData;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+/**
+ * Diese Klasse ist zum Verbinden auf eine Datenbank mittels JDBC und erstellen eines RMs
+ * 
+ * @author lukaszainzinger
+ * @author gbluehberger
+ * @version 2015-01-14
+ */
+
+
+public class JDBCController {
+	private String name;
+	private String server;
+	private String benutzer;
+	private String passwort;
+	private String datenbank;
+	
+	/**
+	 * Konstruktor von Controller
+	 * @param server der Server der verwendet wird
+	 * @param benutzer der benutzer mit den amgemeldet wird
+	 * @param passwort das passwort des benutzer (leer wenn es keines gibt)
+	 * @param datenbank die datenbank die verwendet wird
+	 */
+	public JDBCController(String server,String benutzer,String passwort,String datenbank){
+
+		this.server = server;
+		this.benutzer = benutzer;
+		this.passwort = passwort;
+		this.datenbank = datenbank;
+	}	
+	/**
+	 * Diese Methode liest die PK,FK, Attribute und die Tabellennamen aus der Datenbank aus. 
+	 * @return Liste der PK, FK und Attribute
+	 */
+	public ArrayList<Table> getData(){
+		MysqlDataSource ds = new MysqlDataSource();
+		ds.setServerName(server);
+		ds.setUser(benutzer);
+		ds.setPassword(passwort);
+		ds.setDatabaseName(datenbank);
+		ArrayList<Table> tables = new ArrayList<Table>(); 
+
+		try {
+			Connection con = ds.getConnection();
+			// Abfrage vorbereiten und ausfuÌhren
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("show tables;");
+	
+			while (rs.next()) { // Cursor bewegen
+				Table t = new Table();
+				t.setName(rs.getString(1));// Name der Tabellen auslesen
+				tables.add(t);// in ArrayList speichern
+			}
+			String befehl ="";
+			for(int i = 0; i < tables.size(); i++){
+				befehl="desc "+tables.get(i).getName()+";"; // Befehl aendern auf desc TABLENAME; für die Attribute
+				rs = st.executeQuery(befehl);
+				
+				ArrayList<String> atri = new ArrayList<String>();
+				ArrayList<String> primary = new ArrayList<String>();
+				ArrayList<String> foreign = new ArrayList<String>();//Fremdschlüssel
+
+				while(rs.next()){
+
+					atri.add(rs.getString(1)); // Speichern der Atribute
+					if(rs.getString(4).equals("PRI")){ // Ueberpruefen ob PK.
+						primary.add(rs.getString(1));
+					}
+					if(rs.getString(4).equals("MUL")){ // Ueberpruefen ob FK.
+						foreign.add(rs.getString(1));
+					}
+				}
+				
+				tables.get(i).setAttribute(atri); // Attribute hinzufüen in den Tabellen.
+				tables.get(i).setPkey(primary);
+				tables.get(i).setFkey(foreign);
+			}
+			rs.close(); st.close(); con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tables;
+	}
+}
---
+++ Rueckwaertssalto/src/bluehberger_zainziger/Table.java
@@ -0,0 +1,73 @@
+package bluehberger_zainziger;
+
+/**
+ * Klasse zum Verwalten der Tabellennamen, Attribute und Schlüssel (PK sowie FK).
+ * 
+ * @author lzainzinger
+ * @author gbluehberger
+ * @version 2015-01-14
+ */
+import java.util.ArrayList;
+
+public class Table {
+	private String name;
+	private ArrayList<String> attribute = new ArrayList<String>();
+	private ArrayList<String> pkey = new ArrayList<String>(); // Speichert die Namen der Schluessel
+	private ArrayList<String> fkey = new ArrayList<String>();
+	
+	/**
+	 * Gibt alle Attribute zurück.
+	 * @return the attribute
+	 */
+	public ArrayList<String> getAttribute() {
+		return attribute;
+	}
+	/**
+	 * Setzt die Attribute
+	 * @param attribute the attribute to set
+	 */
+	public void setAttribute(ArrayList<String> attribute) {
+		this.attribute = attribute;
+	}
+	/**
+	 * Gibt die PK zurück
+	 * @return the pkey
+	 */
+	public ArrayList<String> getPkey() {
+		return pkey;
+	}
+	/**
+	 * Setzt die PK
+	 * @param pkey the pkey to set
+	 */
+	public void setPkey(ArrayList<String> pkey) {
+		this.pkey = pkey;
+	}
+	/**
+	 * Gibt die FK zurück
+	 * @return the fkey
+	 */
+	public ArrayList<String> getFkey() {
+		return fkey;
+	}
+	/**
+	 * Setzt die FK
+	 * @param fkey the fkey to set
+	 */
+	public void setFkey(ArrayList<String> fkey) {
+		this.fkey = fkey;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
+
